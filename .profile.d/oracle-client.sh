29908daad07190a3a3ee9dfe2007889ed686f974
Ramazan  Said GALİP <107424417+ramazangalip@users.noreply.github.com>
2025-12-15 03:33:00 +0300

    .

---
+++ .profile.d/oracle-client.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Oracle Instant Client Kurulumu (Buildpack için)
+
+# Gerekli Instant Client sürümü
+ORACLE_INSTANT_CLIENT_VERSION="23.4.0.0.0"
+ORACLE_INSTANT_CLIENT_DIR="/app/.heroku/python/instantclient_${ORACLE_INSTANT_CLIENT_VERSION}"
+
+# Geçici indirme dizini
+mkdir -p $ORACLE_INSTANT_CLIENT_DIR
+
+# Instant Client Basic Package'ı indirme (Linux x64 için)
+curl -o /tmp/instantclient.zip https://download.oracle.com/otn_software/linux/instantclient/234000/instantclient-basiclite-linux.x64-23.4.0.24.05.zip
+
+# İndirilen dosyayı açma
+unzip /tmp/instantclient.zip -d $ORACLE_INSTANT_CLIENT_DIR
+rm /tmp/instantclient.zip
+
+# Oracle Wallet'ın bulunduğu yeri TNS_ADMIN olarak ayarlama
+export TNS_ADMIN=/app/wallet
+
+# LD_LIBRARY_PATH'i ayarlama (oracledb kütüphanesinin ihtiyacı)
+export LD_LIBRARY_PATH=$ORACLE_INSTANT_CLIENT_DIR/instantclient_${ORACLE_INSTANT_CLIENT_VERSION}
